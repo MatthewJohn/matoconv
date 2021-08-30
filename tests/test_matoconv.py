a105b6f473e6834f5115b348d45fac4e30e9cd2e
Matthew John <matthew@dockstudios.co.uk>
2021-08-30 10:46:56 +0100

    Add tests for get_instance for Matoconv

--- tests/test_matoconv.py
+++ tests/test_matoconv.py
@@ -1,16 +1,40 @@
 
-from unittest import TestCase, mock
+from unittest import TestCase
 
-from matoconv import Matoconv
+from matoconv import Matoconv, SingletonNotInstanciatedError
 
 
-class TestMatoconv(TestCase):
+class TestGetInstance(TestCase):
 
-    def test_matoconv(self):
-        pass
+    def setUp(self) -> None:
+        """Store current value of Matoconve INSTANCE static value."""
+        self.original_matoconv_instance_value = Matoconv.INSTANCE
+        return super().setUp()
 
-    def client(self):
-        matoconv = Matoconv()
-        matoconv.app.config['TESTING'] = True
-        with matoconv.app.test_client() as client:
-            yield client
+    def tearDown(self) -> None:
+        """Remove any cached version so Matoconv"""
+        Matoconv.INSTANCE = self.original_matoconv_instance_value
+        return super().tearDown()
+
+    def test_get_without_create(self):
+        """
+        Ensure exception is raised when attempting to get_instance without creating.
+        """
+        self.assertEqual(Matoconv.INSTANCE, None)
+        with self.assertRaises(SingletonNotInstanciatedError):
+            Matoconv.get_instance(create=False)
+        self.assertEqual(Matoconv.INSTANCE, None)
+
+    def test_get(self):
+        """
+        Ensure exception is raised when attempting to get_instance without creating.
+        """
+        # Ensure there is no cached version
+        self.assertEqual(Matoconv.INSTANCE, None)
+        matoconv = Matoconv.get_instance(create=True)
+        # Ensure returned instance is an instance of Matoconv
+        self.assertIsInstance(matoconv, Matoconv)
+
+        # Ensure instance is cached.
+        self.assertEqual(Matoconv.INSTANCE, matoconv)
+
