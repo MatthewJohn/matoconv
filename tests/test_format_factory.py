e9bd47f89f0c407427e29c6d70c3ad2f225f43d5
Matthew John <matthew@dockstudios.co.uk>
2021-08-30 08:54:57 +0100

    Add tests for by_extension

--- tests/test_format_factory.py
+++ tests/test_format_factory.py
@@ -1,15 +1,39 @@
 
 from unittest import TestCase, mock
 
-from matoconv import FormatFactory
+from matoconv import FormatFactory, PDF, ODT, DOC, DOCX, HTML
 
 
 class TestFormatFactory(TestCase):
 
     def setUp(self) -> None:
         self.format_factory = FormatFactory()
+        self.format_factory.register_formats()
         return super().setUp()
 
-    def test_unknown_format(self) -> None:
+    def test_by_extension_unknown(self) -> None:
+        """Test by_extension with an unknown extension."""
         form = self.format_factory.by_extension('doesnotexist')
         self.assertEqual(form, None)
+
+    def test_by_extension(self):
+        """Test by_extension method."""
+        for ext, cls in [
+                ['pdf', PDF], ['docx', DOCX],
+                ['doc', DOC], ['html', HTML],
+                ['odt', ODT]]:
+            self.assertIsInstance(
+                self.format_factory.by_extension(ext),
+                cls
+            )
+
+    def test_by_extension_capital(self):
+        """Test by_extension providing a capitalised extension."""
+        for ext, cls in [
+                ['PDF', PDF], ['DOCX', DOCX],
+                ['DOC', DOC], ['HTML', HTML],
+                ['ODT', ODT]]:
+            self.assertIsInstance(
+                self.format_factory.by_extension(ext),
+                cls
+            )
