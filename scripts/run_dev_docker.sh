7bf0a718c38ad8fde257a4ba45ce001331d977ff
Matthew John <matthew@dockstudios.co.uk>
2021-08-30 13:04:25 +0100

    Update dev script to tail logs, name container and remove local build dirs

--- scripts/run_dev_docker.sh
+++ scripts/run_dev_docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-docker build . -t matoconv-test
-cont=$(docker run -p8091:8091 -d matoconv-test)
+rm -rf build matoconv.egg-info dist
 
+docker build . -t matoconv-test
+docker run -p8091:8091 --name=matoconv-test -d matoconv-test
+
+docker logs -f matoconv-test &
 echo 'Running on :8091. Press <Enter> to stop and destroy'
 read
 
-docker kill $cont
-docker rm $cont
+docker kill matoconv-test
+docker rm matoconv-test
