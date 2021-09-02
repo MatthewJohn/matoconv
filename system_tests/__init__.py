3dbd8e6bd1ae312dbbd6e80102a537eac6bfca5a
Matthew John <matthew@dockstudios.co.uk>
2021-09-02 20:38:59 +0100

    Fix format and input file specified in request sent to matoconv in system tests

--- system_tests/__init__.py
+++ system_tests/__init__.py
@@ -23,54 +23,59 @@ class FileSpec(object):
     def full(self, file):
         return os.path.join(self.cwd, file)
 
-    def templated_name(self, file_type, extension):
-        return self.full(f'{file_type}_{self.name}.{extension}')
+    def templated_name(self, file_type: str, extension: str, with_path: bool):
+        file_ = f'{file_type}_{self.name}.{extension}'
+        return self.full(file_) if with_path else file_
 
     @property
     def input_file(self):
-        return self.templated_name('input', self.input_extension)
+        return self.templated_name('input', self.input_extension, with_path=False)
 
     @property
-    def output_file(self):
-        return self.templated_name('output', self.output_extension)
+    def input_file_path(self):
+        return self.templated_name('input', self.input_extension, with_path=True)
+
+    @property
+    def output_file_path(self):
+        return self.templated_name('output', self.output_extension, with_path=True)
 
     @property
     def output_screenshot(self):
-        return self.templated_name('output', self.screenshot_extension)
+        return self.templated_name('output', self.screenshot_extension, with_path=True)
 
     @property
     def reference_output_screenshot(self):
-        return self.templated_name('reference', self.screenshot_extension)
+        return self.templated_name('reference', self.screenshot_extension, with_path=True)
 
     @property
     def reference_deltas(self):
-        return self.templated_name('input_output_diff', 'txt')
+        return self.templated_name('input_output_diff', 'txt', with_path=True)
 
     @property
     def input_screenshot(self):
-        return self.templated_name('input', self.screenshot_extension)
+        return self.templated_name('input', self.screenshot_extension, with_path=True)
 
 
 class TestByExtension(TestRouteBase):
 
     def _convert_file(self, file_spec: FileSpec):
-        with open(file_spec.input_file, 'rb') as fh:
+        with open(file_spec.input_file_path, 'rb') as fh:
             in_data = fh.read()
         with self.client as client:
             res = client.post(
-                'http://localhost:8091/convert/format/docx',
-                headers={'Content-Disposition': 'attachment; filename="input_a.odt"'},
+                f'http://localhost:8091/convert/format/{file_spec.output_extension}',
+                headers={'Content-Disposition': f'attachment; filename="{file_spec.input_file}"'},
                 data=in_data)
 
         self.assertEqual(res.status_code, 200)
 
-        with open(file_spec.output_file, 'wb') as fh:
+        with open(file_spec.output_file_path, 'wb') as fh:
             fh.write(res.data)
 
     def _screenshot_files(self, file_spec: FileSpec):
         for file_, extension, screenshot_file in [
-                [file_spec.input_file, file_spec.input_extension, file_spec.input_screenshot],
-                [file_spec.output_file, file_spec.output_extension, file_spec.output_screenshot]]:
+                [file_spec.input_file_path, file_spec.input_extension, file_spec.input_screenshot],
+                [file_spec.output_file_path, file_spec.output_extension, file_spec.output_screenshot]]:
             if extension in ['doc', 'docx', 'odt']:
                 cmd = ['libreoffice', '--convert-to', 'jpg', file_]
 
@@ -138,11 +143,11 @@ class TestByExtension(TestRouteBase):
     def test_comparison_a_odt_docx(self):
         self._perform_test('a', 'odt', 'docx')
 
-    def test_comparison_a_odt_doc(self):
+    def test_comparison_b_odt_doc(self):
         self._perform_test('b', 'odt', 'doc')
 
-    def test_comparison_a_odt_pdf(self):
+    def test_comparison_c_odt_pdf(self):
         self._perform_test('c', 'odt', 'pdf')
 
-    def test_comparison_a_odt_html(self):
+    def test_comparison_d_odt_html(self):
         self._perform_test('d', 'odt', 'html')
